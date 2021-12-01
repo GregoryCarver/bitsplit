983b21dddfad3c411bd82a5563608665a617cf51
Crunkmeiser <chenesy@ufl.edu>
2021-12-01 12:49:58 -0500

    Update Logger.java

--- src/Logger.java
+++ src/Logger.java
@@ -10,6 +10,7 @@ public class Logger
     public static void log(int peerID, int expectedPeerID, String message, int type)
     {
         try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
             String logFile = "log_peer_" + String.valueOf(peerID) + ".log";
             FileWriter fw = new FileWriter(logFile, true);
             BufferedWriter bw = new BufferedWriter(fw);
@@ -21,15 +22,31 @@ public class Logger
                 LocalTime time1 = LocalTime.now();
                 fw.write(time1.format(formatter) + ": Peer " + expectedPeerID + " is unchoked by " + peerID);
                 fw.close();
-                System.out.println("Successfully wrote to " + logfile + ".");
+                System.out.println("Successfully wrote to " + logFile + ".");
                     break;
                 case 1:   
+                    LocalTime time1 = LocalTime.now();
+                    fw.write(time1.format(formatter) + ": Peer " + expectedPeerID + " is choked by " + peerID);
+                    fw.close();
+                    System.out.println("Successfully wrote to " + logFile + ".");
                     break;
                 case 2:   
+                    LocalTime time1 = LocalTime.now();
+                    fw.write(time1.format(formatter) + ": Peer " + expectedPeerID + " is interested in " + peerID);
+                    fw.close();
+                    System.out.println("Successfully wrote to " + logFile + ".");
                     break;
                 case 3:
+                    LocalTime time1 = LocalTime.now();
+                    fw.write(time1.format(formatter) + ": Peer " + expectedPeerID + " is not interested in " + peerID);
+                    fw.close();
+                    System.out.println("Successfully wrote to " + logFile + ".");
                     break;
                 case 4:
+                    LocalTime time1 = LocalTime.now();
+                    fw.write(time1.format(formatter) + ": Peer " + expectedPeerID + " has " + peerID);
+                    fw.close();
+                    System.out.println("Successfully wrote to " + logFile + ".");
                     break;
                 case 5:
                     break;

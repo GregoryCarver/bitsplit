f38b040e0665cb94abc1563849f1b97a5c438fe9
Crunkmeiser <chenesy@ufl.edu>
2021-12-01 12:42:30 -0500

    Update Logger.java

--- src/Logger.java
+++ src/Logger.java
@@ -5,7 +5,7 @@ import java.io.PrintWriter;
 
 public class Logger
 {
-    public static void log(int peerID, int expectedPeerID, String message, String type)
+    public static void log(int peerID, int expectedPeerID, String message, int type)
     {
         try {
             FileWriter fw = new FileWriter("log_peer_" + String.valueOf(peerID) + ".log", true);
@@ -14,9 +14,21 @@ public class Logger
             out.println("Peer " + String.valueOf(peerID) + ": " + message);
             out.close();
             switch (type) {
-                case "choke": 
+                case 0: 
                     break;
-                case "balls":   
+                case 1:   
+                    break;
+                case 2:   
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
                     break;
 
             }
